a9683aff9ca1beaa47d2e17af3d8cb4c51acf3da
Huyshadow <107664507+Huyshadow@users.noreply.github.com>
2023-12-01 13:15:51 +0700

    Update urls.py

--- user_service/urls.py
+++ user_service/urls.py
@@ -6,6 +6,5 @@ urlpatterns = [
     path("register/", apis.RegisterApi.as_view(), name="register"),
     path("login/", apis.LoginApi.as_view(), name="login"),
     path("profile/", apis.UserApi.as_view(), name="profile"),
-    # path("profile/update", apis.UserUpdateInfoApi.as_view(), name="update_profile"),
     path("logout/", apis.LogoutApi.as_view(), name="logout"),
 ]
