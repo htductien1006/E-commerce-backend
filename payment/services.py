730e21108057eb2accce604c76a3e4735dfc82e0
htductien1006 <96772199+htductien1006@users.noreply.github.com>
2023-12-07 00:08:43 +0700

    payment api done

--- payment/services.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import get_object_or_404
-from product_service import models
-from user_service.models import ShoppingSession, User
-
-def get_payment_amount(shoppingsession_data): 
-    amount = shoppingsession_data.total
-    payment_id = shoppingsession_data.payment_id
-    
