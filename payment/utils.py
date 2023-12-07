39fc613e7e91ae7b7c32f4898985f72805d0aa29
Huy Dang <huy.dang110902@gmail.com>
2023-12-07 12:58:36 +0700

    Finish

--- payment/utils.py
+++ payment/utils.py
@@ -2,11 +2,12 @@ import requests
 import json
 from decouple import config
 
+
 def make_paypal_payment(amount, currency, return_url, cancel_url):
     # Set up PayPal API credentials
     client_id = config("PAYPAL_ID")
     secret = config("PAYPAL_SECRET")
-    url =config("PAYPAL_BASE_URL")
+    url = config("PAYPAL_BASE_URL")
 
     # Set up API endpoints
     base_url = url
@@ -16,13 +17,13 @@ def make_paypal_payment(amount, currency, return_url, cancel_url):
     # Request an access token
     token_payload = {'grant_type': 'client_credentials'}
     token_headers = {'Accept': 'application/json', 'Accept-Language': 'en_US'}
-    token_response = requests.post(token_url, auth=(client_id, secret), data=token_payload, headers=token_headers)
+    token_response = requests.post(token_url, auth=(
+        client_id, secret), data=token_payload, headers=token_headers)
 
     if token_response.status_code != 200:
-        return False,"Failed to authenticate with PayPal API",None
-    
+        return False, "Failed to authenticate with PayPal API", None
+
     access_token = token_response.json()['access_token']
-    print(access_token)
 
     # Create payment payload
     payment_payload = {
@@ -44,38 +45,39 @@ def make_paypal_payment(amount, currency, return_url, cancel_url):
         'Authorization': f'Bearer {access_token}'
     }
 
-    payment_response = requests.post(payment_url, data=json.dumps(payment_payload), headers=payment_headers)
-    print("payment_response: \n", payment_response.text)
+    payment_response = requests.post(payment_url, data=json.dumps(
+        payment_payload), headers=payment_headers)
     if payment_response.status_code != 201:
-        return False , 'Failed to create PayPal payment.',None
+        return False, 'Failed to create PayPal payment.', None
 
     paypal_id = payment_response.json()['id']
-    approval_url = next(link['href'] for link in payment_response.json()['links'] if link['rel'] == 'approval_url')
+    approval_url = next(link['href'] for link in payment_response.json()[
+                        'links'] if link['rel'] == 'approval_url')
 
     return True, paypal_id, approval_url
+
 
 def verify_paypal_payment(paypal_id, payer_id):
     # Set up PayPal API credentials
     client_id = config("PAYPAL_ID")
     secret = config("PAYPAL_SECRET")
-    url =config("PAYPAL_BASE_URL")
+    url = config("PAYPAL_BASE_URL")
 
     # Set up API endpoints
     base_url = url
     token_url = base_url + '/v1/oauth2/token'
     payment_url = base_url + f'/v1/payments/payment'
-    print(payment_url)
 
     # Request an access token
     token_payload = {'grant_type': 'client_credentials'}
     token_headers = {'Accept': 'application/json', 'Accept-Language': 'en_US'}
-    token_response = requests.post(token_url, auth=(client_id, secret), data=token_payload, headers=token_headers)
+    token_response = requests.post(token_url, auth=(
+        client_id, secret), data=token_payload, headers=token_headers)
 
     if token_response.status_code != 200:
         raise Exception('Failed to authenticate with PayPal API.')
 
     access_token = token_response.json()['access_token']
-    print(access_token)
 
     # Retrieve payment details
     payment_headers = {
@@ -84,8 +86,9 @@ def verify_paypal_payment(paypal_id, payer_id):
     }
 
     payment_details_url = f'{payment_url}/{paypal_id}/execute'
-    data = { "payer_id": f'{payer_id}' }
-    payment_details_response = requests.post(payment_details_url, data=json.dumps(data) ,headers=payment_headers)
+    data = {"payer_id": f'{payer_id}'}
+    payment_details_response = requests.post(
+        payment_details_url, data=json.dumps(data), headers=payment_headers)
 
     print(payment_details_response.status_code)
 
